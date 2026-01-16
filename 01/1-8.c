8430819a5d1ae3b9a18436b7ffddf7442f690f2f
brave0duck <newkemp@naver.com>
2026-01-16 20:52:51 +0900

    1-16-2026 pm8:53 at home

---
+++ 01/1-8.c
@@ -0,0 +1,23 @@
+// 직각 이등변 삼각형 출력
+#include <stdio.h>
+
+int main(void)
+{
+    int i, j, n;
+    do
+    {
+        printf("몇 단 삼각형 입니까? : ");
+        scanf("%d", &n);
+
+    } while (n <= 0);
+
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            putchar('*');
+        }
+        putchar('\n');
+    }
+    return 0;
+}
--- 01/ex-q16.c
+++ 01/ex-q16.c
@@ -1,14 +1,27 @@
 // require : 직각이등변 삼각형을 출력하는 함수 (*로)
 #include <stdio.h>
-void triangleLB(int n){
-    for(int i=1; i<=n; i++){
-        for(int j=1; j<=i; j++){
+
+void triangleLB(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
             printf("*");
         }
         putchar('\n');
     }
 }
-int main(void){
-    
+int main(void)
+{
+    int n;
+    do
+    {
+        printf("몇 단 삼각형 입니까? : ");
+        scanf("%d", &n);
+    } while (n <= 0);
+
+    triangleLB(n);
+
     return 0;
 }
