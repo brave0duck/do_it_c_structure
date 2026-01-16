7d5f3742acb19d474aec558e6ad49a61640347d2
brave0duck <newkemp@naver.com>
2026-01-16 19:52:18 +0900

    --

--- 01/1-4.c
+++ 01/1-4.c
@@ -6,36 +6,29 @@ int sum_to_n(int n);
 int main(void)
 {
     int input;
-    int result;
+    long result;
 
     printf("sum 1 to N. input N (0 to exit): ");
     while (scanf("%d", &input) != EOF && input != 0)
     {
         result = sum_to_n(input);
-        if (result != -1)
-        {
-            printf("sum result : %d\n", result);
-        }
-        else
-        {
+        if (result != -1){
+            printf("sum result : %ld\n", result);
+        }else{
             printf("%d is must bigger than 0.\n", input);
         }
         printf("sum 1 to N. input N (0 to exit): ");
     }
-    printf("Program exit.");
+    printf("\nProgram exit.\n");
     return 0;
 }
 int sum_to_n(int n)
 {
-    int sum = 0;
-    if (n < 1)
-    {
+    long sum = 0;
+    if (n < 1){
         return -1;
-    }
-    else
-    {
-        for (int i = 1; i <= n; i++)
-        {
+    }else{
+        for (int i = 1; i <= n; i++){
             sum += i;
         }
         return sum;
---
+++ 01/ex-q6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+    int n,i;
+    int sum;
+    puts("1부터 n까지의 합을 구합니다.");
+    printf("n의 값 : ");
+    scanf("%d",&n);
+
+    sum=0;
+    i=1;
+    
+    return 0;
+}
