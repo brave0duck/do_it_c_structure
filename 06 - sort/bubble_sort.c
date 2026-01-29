ee924eeafbed7430e1fa0f69040c2fe4b0507461
brave0duck <brave0duck@naver.com>
2026-01-29 18:34:03 +0900

    --

--- 06 - sort/bubble_sort.c
+++ 06 - sort/bubble_sort.c
@@ -4,13 +4,13 @@
 #include <time.h>
 #define __MAX__ 10
 
-void bubble_sort(const int a[], int size){
+void bubble_sort(int a[], int size){
     for(int i=0; i<size; i++){
-        for(int j=i+1; j<size; j++){
-            if (a[i] > a[j]){
+        for(int j=size-1; j>i; j--){
+            if (a[j-1] > a[j]){
                 int tmp;
-                tmp = a[i];
-                a[i] = a[j];
+                tmp = a[j-1];
+                a[j-1] = a[j];
                 a[j] = tmp;
             }
         }
@@ -29,10 +29,10 @@ int main(void){
 
     bubble_sort(a,__MAX__);
 
-    printf("After Bubble sort : \t");
+    printf("\nAfter Bubble sort : \t");
     for(int i=0; i<__MAX__; i++){
-        a[i] = 1 + rand() % 100;
         printf("%d ", a[i]);
     }
+    putchar('\n');
     return 0;
 }
